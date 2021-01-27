400222fa6276d88bbf04349a3d13b1ac0771713e
Kostoeva <irmareka@gmail.com>
2021-01-26 21:57:44 -0800

    Adding skeleton for fetching tweets by user handle

--- main.py
+++ main.py
@@ -7,4 +7,18 @@ from secrets import *
 t = Twitter(auth=OAuth(token, token_secret, consumer_key, consumer_secret))
 
 # Retreiving Twitter mentions with chess moves
-chessMoves = t.
+#chessMoves = t.
+
+# Hard coding for now; later can take as input or whatever is necessary
+USER_ID = "Chessbot4" 
+
+# Wrapping Twitter GETs in a try/catch in case we encounter an error/exception
+try:
+    tweets = t.statuses.user_timeline(screen_name=USER_ID)
+    for t in tweets:
+        print(t)
+
+# Catching an exception; look up API docu on more specific exceptions if we want to addr them case-by-case
+except Exception as e:
+    print('Exception')
+    print(e)
