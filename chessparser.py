9aad9bd19968626d0dd43c22681866da2177289b
Nate <flyingwithnate@pm.me>
2021-01-29 12:55:39 -0800

    cheatsheet + pychess tests

--- chessparser.py
+++ chessparser.py
@@ -2,9 +2,9 @@ import chess
 import chess.pgn
 import io
 
-pgn = open("fischer_v_spassky.pgn")
-
-first_game = chess.pgn.read_game(pgn)
+def openPGN():
+    pgn = open("fischer_v_spassky.pgn")
+    first_game = chess.pgn.read_game(pgn)
+    return first_game
 
 print(first_game)
-
